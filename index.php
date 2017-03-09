a81447ad0b5063ed8a0c5417e0ba150f78a82948
RoseLand96 <1716210001@utec-tgo.edu.mx>
2017-03-08 17:57:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 mysqli_select_db($db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
     @mysqli_query("SET NAMES 'utf8'");
 
-$sql_query = "SELECT * FROM contactos;";
+$sqli_query = "SELECT * FROM contactos;";
 $result = mysqli_query($sqli_query);
 $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
